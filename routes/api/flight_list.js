0369161470a7dbf13a6b124c27f8b26f26c80890
Jaffer Ali <jafferali150@gmail.com>
2021-05-02 20:26:33 +0530

    added delete and update sql flight_list data

--- routes/api/flight_list.js
+++ routes/api/flight_list.js
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 // @desc    GET By id
 // @access  Public
 router.get('/:id', (req, res) => {
-    const query = `SELECT * FROM flight_list where flight_id=${req.params.id};`
+    const query = `SELECT * FROM flight_list WHERE flight_id=${req.params.id};`
     db.query(query, (err, result) => {
         res.send(result[0]);
     })
@@ -45,5 +45,47 @@ router.post('/', (req, res) => {
         res.send(result);
     })
 })
+
+// @route   DELETE api/aircraft_list
+// @desc    DELETE By id
+// @access  Public
+router.delete('/:id', (req, res) => {
+    const query = `DELETE FROM flight_list WHERE flight_id = ${req.params.id};`
+    db.query(query, (err, result) => {
+        if (err || result.affectedRows === 0) {
+            res.status(404).json({success: false});
+        }
+        else {
+            res.status(200).json({success: true});
+        }
+    })
+})
+
+// @route   UPDATE api/aircraft_list
+// @desc    UPDATE By id
+// @access  Public
+router.put('/:id', (req, res) => {
+    const query = `UPDATE flight_list SET flight_id = ?, airlines_id = ?, flight_name = ?, departure_airport_id = ?, arrival_airport_id = ?, departure_datetime = ?, arrival_datetime = ?, available_seats = ?, price = ? WHERE (flight_id = ?);`
+    db.query(query,[
+        req.body.flight_id, 
+        req.body.airlines_id,
+        req.body.flight_name,
+        req.body.departure_airport_id,
+        req.body.arrival_airport_id,
+        req.body.departure_datetime,
+        req.body.arrival_datetime,
+        req.body.available_seats,
+        req.body.price,
+        req.params.id
+    ], (err, result) => {
+        if (err || result.affectedRows === 0) {
+            res.status(404).json({success: false});
+        }
+        else {
+            res.status(200).json({success: true});
+        }
+    })
+})
+
 
 module.exports = router;
